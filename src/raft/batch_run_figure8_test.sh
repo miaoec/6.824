e72ecf6b409affc642446c713f84fdd87c87acb9
regmiao <regmiao@tencent.com>
2022-11-15 21:31:47 +0800

    feat:add batch sh

--- src/raft/batch_run_figure8_test.sh
+++ src/raft/batch_run_figure8_test.sh
@@ -3,7 +3,7 @@ mkdir batch_test_tmp
 for i in {1..30}
   do go test -run TestFigure82C > batch_test_tmp/batch_run_figure8_testlog_$i
   done
-cat batch_test_tmp/batch_run_figure8_testlog_* |grep "FAIL:" -A 3
+cat batch_test_tmp/batch_run_figure8_testlog_* |grep "FAIL:" -A 3 > failed.log
 
 # ps -ef | grep TestFigure82C | awk '{print $2}' | xargs kill -9
 
